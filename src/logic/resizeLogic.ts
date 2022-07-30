8a004f99978952a78f415841db372a7695f11818
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-07-30 19:21:52 +0200

    adding resized folder if not exist

--- src/logic/resizeLogic.ts
+++ src/logic/resizeLogic.ts
@@ -12,29 +12,37 @@ const displayImage =async (imageName:string,imageWidth:number,imageHeight:number
         //original image withot given resizing width or height
         if(!imageWidth && !imageHeight)
         return myImg;
-        //resize image with only width
-        else if (!imageHeight){
-            const newImage=path.join(myImg,`../../resized/${imageName}_width-${imageWidth}.jpg`);
-            if(!fs.existsSync(newImage)){
-                await sharp(myImg).resize({width}).toFile(newImage);
+        else{
+            //crearting resized folder if not exist
+            const resizeFolder=path.join(__dirname,'../../images/resized')
+            if(!fs.existsSync(resizeFolder)){
+                fs.mkdirSync(resizeFolder);
             }
-            return newImage;
+            //resize image with only width
+            if (!imageHeight){
+                const newImage=path.join(resizeFolder,`${imageName}_width-${imageWidth}.jpg`);
+                if(!fs.existsSync(newImage)){
+                    await sharp(myImg).resize({width}).toFile(newImage);
+                }
+                return newImage;
+            }
+            //resize image with only height
+            else if (!imageWidth){
+                const newImage=path.join(resizeFolder,`${imageName}_height-${imageHeight}.jpg`);
+                if(!fs.existsSync(newImage)){
+                    await sharp(myImg).resize({height}).toFile(newImage);
+                }
+                return newImage;
+            }
+            //resize image with width and height
+            else {
+                const newImage=path.join(resizeFolder,`${imageName}_width-${imageWidth}_height-${imageHeight}.jpg`);
+                if(!fs.existsSync(newImage)){
+                    await sharp(myImg).resize({width,height}).toFile(newImage);
+                }
+                return newImage
+            }
         }
-        //resize image with only height
-        else if (!imageWidth){
-            const newImage=path.join(myImg,`../../resized/${imageName}_height-${imageHeight}.jpg`);
-            if(!fs.existsSync(newImage)){
-                await sharp(myImg).resize({height}).toFile(newImage);
-            }
-            return newImage;
-        }
-        //resize image with width and height
-        else {
-            const newImage=path.join(myImg,`../../resized/${imageName}_width-${imageWidth}_height-${imageHeight}.jpg`);
-            if(!fs.existsSync(newImage)){
-                await sharp(myImg).resize({width,height}).toFile(newImage);
-            }
-            return newImage}; 
     }
     else  return "file not exist";
 }
