b4bea5b31c05b4ff657e9fd8b448e54cf52dd342
samarkhaled4 <samarkhaled.iti@gmail.com>
2022-07-31 11:02:18 +0200

    Editing in handling errors of width & height values

--- src/routes/api/main.ts
+++ src/routes/api/main.ts
@@ -7,30 +7,20 @@ apiRoute.get('/', (req: Request, res: Response) => {
     res.send('api connectd')
 })
 apiRoute.get('/images', async (req: Request, res: Response): Promise<void> => {
-    const filename = req.query.filename as string
-    const width = parseInt(req.query.width as string)
-    const height = parseInt(req.query.height as string)
 
-    //check validation of width and height values
-    //check validity of width value
-    if (isNaN(width) && !req.query.height && req.query.width) {
-        res.status(400).send('Please insert valid values for width !!')
-    }
-    //check validity of height value
-    else if (isNaN(height) && !req.query.width && req.query.height) {
-        res.status(400).send('Please insert valid values for height !!')
-    }
+    const filename = req.query.filename as string
+    const w=req.query.width as string;
+    const h=req.query.height as string;
+
     //check validity of both width and height values
-    else if (
-        isNaN(height) &&
-        isNaN(width) &&
-        req.query.height &&
-        req.query.width
-    ) {
+    if ( (h && !/^([1-9]\d*)$/.test(h)) || (w && !/^([1-9]\d*)$/.test(w)) ) {
         res.status(400).send(
-            'Please insert valid values for width and height !!'
+            'Please insert only positive integers for width and height !!'
         )
-    } else {
+    } 
+    else {
+        const width = parseInt(w);
+        const height = parseInt(h);
         if (filename) {
             const path = await displayImage(filename, width, height)
             //I have already done logic of caching inside displayImage function itself
@@ -42,7 +32,8 @@ apiRoute.get('/images', async (req: Request, res: Response): Promise<void> => {
                     "Image name isn't exist , please enter right one !"
                 )
             }
-        } else {
+        } 
+        else {
             res.status(400).send(
                 'Could you enter filename parameter in query string with valid image name !?'
             )
